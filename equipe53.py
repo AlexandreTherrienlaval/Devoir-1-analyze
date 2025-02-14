1551fd8636637750f03782e1829a9779557849c5
Zack Moisan <zack.moisan.1@ulaval.ca>
2025-02-13 22:36:45 -0500

    version 6

--- equipe53.py
+++ equipe53.py
@@ -13,16 +13,18 @@ K = np.diag(b)
 L = 55*I - J + 2*a*c
 M = 1*K ; M[:,0] = np.reshape(a, (1,6))
 dd = np.linalg.det(M)
+x = np.linalg.solve(M, a)
 
 
-print(f'a =\n{a}\n\n')          # a
-print(f'b =\n{b}\n\n')          # b
-print(f'c =\n{c}\n\n')          # c
-print(f'd =\n{d}\n\n')          # d
-print(f'I =\n{I}\n\n')          # I
-print(f'J =\n{J}\n\n')          # J
-print(f'K =\n{K}\n\n')          # K
-print(f'L =\n{L}\n\n')          # L
-print(f'M =\n{M}\n\n')          # M
-print(f'dd =\n{dd}\n\n')        # dd
+print(f'a =\n{a}\n')
+print(f'b =\n{b}\n')
+print(f'c =\n{c}\n')
+print(f'd =\n{d}\n')
+print(f'I =\n{I}\n')
+print(f'J =\n{J}\n')
+print(f'K =\n{K}\n')
+print(f'L =\n{L}\n')
+print(f'M =\n{M}\n')
+print(f'dd =\n{dd}\n')
+print(f'x =\n{x}\n')
 
