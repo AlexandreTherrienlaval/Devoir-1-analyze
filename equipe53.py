63551f8c2f7a01c6286e3dfc795ee16ec5f20e67
Zack Moisan <zack.moisan.1@ulaval.ca>
2025-02-14 12:30:01 -0500

    version 15

--- equipe53.py
+++ equipe53.py
@@ -20,7 +20,6 @@ dd = int(np.linalg.det(M))
 x = np.array(np.linalg.solve(M, a), 'i')
 N = np.linalg.solve(M, np.transpose(M))
 
-'''
 print(f'a =\n{a}\n')
 print(f'b =\n{b}\n')
 print(f'c =\n{c}\n')
@@ -34,26 +33,18 @@ print(f'dd =\n{dd}\n')
 print(f'x =\n{x}\n')
 print(f'N =\n{N}\n')
 
-'''
-
-'''
 figure_1 = plt.matshow(N)
 plt.title('Matrice N')
 plt.show()
-
-'''
 
 def f(x):
     return -1 * (x**2)/2 + np.exp(x) + np.sin(x)
 
 x = np.linspace(0, 1, 101)
 
-'''
 figure_2 = plt.plot(x, f(x))
 plt.title('f(x) = -x²/2 + e^x + sin(x)')
 plt.show()
-
-'''
 
 
 
@@ -64,14 +55,11 @@ def S(n):
 
 x = [i for i in range(1,20)]
 
-'''
 figure_3 = plt.plot(x, S(x), 'o')
 plt.xlabel('n')
 plt.ylabel('S')
 plt.title('Sn en fonction de n')
 plt.show()
-
-'''
 
 
 
@@ -87,11 +75,9 @@ h = np.array([])
 for i in range(-1, -7, -1):
     h = np.append(h, 10**i)
 
-'''
 figure_4 = plt.loglog(h, abs(fprime(0) - D(0,h)))
 plt.xlabel('h')
 plt.ylabel('Erreur absolue')
 plt.title("Erreur absolue de l'approximation de f(x) en fonction de h")
 plt.show()
-'''
 
