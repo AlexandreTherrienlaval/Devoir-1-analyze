a20794e5cec27c8d75afa490c33f04906bb30388
Zack Moisan <zack.moisan.1@ulaval.ca>
2025-02-13 23:40:08 -0500

    version 7

--- equipe53.py
+++ equipe53.py
@@ -7,13 +7,14 @@ a = np.full((6,1), 1)
 b = np.arange(1,7)
 c = np.reshape(a, (1,6))
 d = c * 53
-I = np.identity(6)
+I = np.identity(6, 'i')
 J = np.full((6,6), 1)
 K = np.diag(b)
 L = 55*I - J + 2*a*c
 M = 1*K ; M[:,0] = np.reshape(a, (1,6))
-dd = np.linalg.det(M)
-x = np.linalg.solve(M, a)
+dd = int(np.linalg.det(M))
+x = np.array(np.linalg.solve(M, a), 'i')
+N = np.linalg.solve(M, np.transpose(M))
 
 
 print(f'a =\n{a}\n')
@@ -27,4 +28,5 @@ print(f'L =\n{L}\n')
 print(f'M =\n{M}\n')
 print(f'dd =\n{dd}\n')
 print(f'x =\n{x}\n')
+print(f'N =\n{N}\n')
 
