231351e995c4d7728a0ed479b5dd6e5437b25338
Zack Moisan <zack.moisan.1@ulaval.ca>
2025-02-13 22:32:07 -0500

    version 5

--- equipe53.py
+++ equipe53.py
@@ -3,24 +3,26 @@ import matplotlib.pyplot as plt
 #from suiteSn import suiteSn
 
 
-a = np.full((6,1),1)
+a = np.full((6,1), 1)
 b = np.arange(1,7)
 c = np.reshape(a, (1,6))
 d = c * 53
 I = np.identity(6)
 J = np.full((6,6), 1)
 K = np.diag(b)
-L = 55*I - J + 2 * a * c
-M = 1 * K ; M[:,0] = np.reshape(a,(1,6))
+L = 55*I - J + 2*a*c
+M = 1*K ; M[:,0] = np.reshape(a, (1,6))
 dd = np.linalg.det(M)
 
 
-print(a)
-print(f'b= {b}')
-print(c)
-print(d)
-print(I)
-print(J)
-print(f'K= {K}')
-print(L)
-print(M)
+print(f'a =\n{a}\n\n')          # a
+print(f'b =\n{b}\n\n')          # b
+print(f'c =\n{c}\n\n')          # c
+print(f'd =\n{d}\n\n')          # d
+print(f'I =\n{I}\n\n')          # I
+print(f'J =\n{J}\n\n')          # J
+print(f'K =\n{K}\n\n')          # K
+print(f'L =\n{L}\n\n')          # L
+print(f'M =\n{M}\n\n')          # M
+print(f'dd =\n{dd}\n\n')        # dd
+
