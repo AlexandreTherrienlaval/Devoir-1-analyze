312d7ccd44f0ea65dcba65fb6a7905d9beaa911b
Alexandre Therrien <althe83@ulaval.ca>
2025-02-14 02:27:31 -0500

    Version 11

--- equipe53.py
+++ equipe53.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from suiteSn import suiteSn
+from math import e
 
 
 a = np.full((6,1), 1)
@@ -16,7 +17,7 @@ dd = np.linalg.det(M)
 x = np.linalg.solve(M, a)
 M_transpose = np.transpose(M) ; N = np.linalg.solve(M, M_transpose)
 
-print(a)
+'''print(a)
 print(b)
 print(c)
 print(d)
@@ -27,29 +28,44 @@ print(L)
 print(M)
 print(dd)
 print(x)
-print(M_transpose)
+print(M_transpose)'''
 
-figure_1 = plt.matshow(N)
+'''figure_1 = plt.matshow(N)
 plt.title('Matrice N')
-plt.show()
+plt.show()'''
 
 def f(x):
     return -1 * (x**2)/2 + np.exp(x) + np.sin(x)
-x = np.linspace(0, 1, 101)
-figure_2 = plt.plot(x, f(x))
-plt.title('f(x) = -x^2/2 + exp(x) + sin(x)')
-plt.show()
 
+x = np.linspace(0, 1, 101)
+'''figure_2 = plt.plot(x, f(x))
+plt.title('f(x) = -x^2/2 + exp(x) + sin(x)')
+plt.show()'''
 
 def S(n):
     return suiteSn(19)[n]
 
 x = [i for i in range(1,20)]
-figure_3 = plt.plot(x, S(x), 'o')
+'''figure_3 = plt.plot(x, S(x), 'o')
 plt.xlabel('S')
 plt.ylabel('n')
 plt.ylabel('S en fonction de n')
+plt.show()'''
+
+def fprime(x):
+    return -1 * x + e**x + np.cos(x)
+
+def D(x, h):
+    return (f((x+h)) - f(x))/h
+
+h = np.array([])
+for i in range(-6,-0):
+    h= np.append(h, 10**i)
+
+
+Figure = plt.loglog(h, abs(fprime(0) - D(0,h) ))
+plt.xlabel('Erreur absolue')
+plt.ylabel('h')
+plt.title('''Erreur absolue de l'approximation de f(x) selon h''')
 plt.show()
-
-
 
